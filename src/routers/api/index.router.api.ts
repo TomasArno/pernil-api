70ef1a07455b23f73ca54036c88da7de1a3bb96f
Tomas <arnotomas1@gmail.com>
2024-02-24 22:11:42 -0300

    regex corrected

--- src/routers/api/index.router.api.ts
+++ src/routers/api/index.router.api.ts
@@ -1,14 +1,14 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import usersRouter from "./users.router.api";
-import clientsRouter from "./clients.router.api";
-import suppliersRouter from "./suppliers.router.api";
-import categoriesRouter from "./categories.router.api";
-import productsRouter from "./products.router.api";
-import movementsRouter from "./movements.router.api";
-import authRouter from "./auth.router.api";
+import usersRouter from './users.router.api';
+import clientsRouter from './clients.router.api';
+import suppliersRouter from './suppliers.router.api';
+import categoriesRouter from './categories.router.api';
+import productsRouter from './products.router.api';
+import movementsRouter from './movements.router.api';
+import authRouter from './auth.router.api';
 
-import handleWrongMethod from "../../middlewares/wrongMethodHandler.mid";
+import handleWrongMethod from '../../middlewares/wrongMethodHandler.mid';
 
 // ROUTER
 
@@ -16,18 +16,18 @@ const router = Router();
 
 // ROUTES
 
-router.use("/auth", authRouter);
-router.use("/users", usersRouter);
-router.use("/clients", clientsRouter);
-router.use("/suppliers", suppliersRouter);
-router.use("/products", productsRouter);
-router.use("/movements", movementsRouter);
-router.use("/categories", categoriesRouter);
+router.use('/auth', authRouter);
+router.use('/users', usersRouter);
+router.use('/clients', clientsRouter);
+router.use('/suppliers', suppliersRouter);
+router.use('/products', productsRouter);
+router.use('/movements', movementsRouter);
+router.use('/categories', categoriesRouter);
 
 // HANDLERS
 
 router.all(
-  "/auth|users|clients|suppliers|categories|products|movements/",
+  /auth|users|clients|suppliers|categories|products|movements/,
   handleWrongMethod
 );
 
